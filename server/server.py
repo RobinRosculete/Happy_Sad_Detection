8f4021efa1d63c975cb59df6c30324db7fcb7bf7
RobinRosculete <robin.rosculete.860@my.csun.edu>
2023-09-02 18:18:21 -0700

    Added Video Streaming from the server to the client

--- server/server.py
+++ server/server.py
@@ -1,24 +1,32 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
-import datetime
+from flask import Flask, Response
+import cv2 as cv
 
-x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
 
+# Funciton For webcam streaming
 
-# Route for seeing a data
-@app.route('/data')
-def get_time():
 
-    # Returning an api for showing in  reactjs
-    return {
-        'Name': "Robin",
-        "Age": "24",
-        "Date": x,
-        "programming": "python"
-    }
+def webcam():
+    camera = cv.VideoCapture(0)
+
+    while True:
+        success, frame = camera.read()
+        if success:
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            camera.release()
+# Route for send
+
+
+@app.route('/webcam')
+def webcam_diplay():
+    return Response(webcam(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 # Running app
